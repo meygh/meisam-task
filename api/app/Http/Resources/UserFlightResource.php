e3f4970c5b57947db44db8ef9714eee67d9bdb64
Meisam Ghanbari <m.q.php.98@gmail.com>
2023-12-27 11:42:47 +0330

    Add phpdocs annotations in purpose of IDE guides

--- api/app/Http/Resources/UserFlightResource.php
+++ api/app/Http/Resources/UserFlightResource.php
@@ -5,6 +5,14 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * Attributes:
+ * @property int $user_id
+ * @property string $departure_airport
+ * @property string $destination_airport
+ * @property string $created_at
+ * @property string $updated_at
+ */
 class UserFlightResource extends JsonResource
 {
     /**
